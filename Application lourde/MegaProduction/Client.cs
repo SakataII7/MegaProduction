7b54d4cba7acf457a497801e26db51f596e6cf76
Valentin Maubian <valentin.maubian@gmail.com>
2016-03-29 13:41:15 +0200

    Reorganize files

---
+++ Application lourde/MegaProduction/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaProduction
+{
+    class Client
+    {
+        #region Fields
+        private string _Libelle;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+    }
+}
